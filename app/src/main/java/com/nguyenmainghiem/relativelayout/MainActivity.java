55da3b456ba7452facab2fcd2cf6c9e03bede6f8
NguyenMaiNghiem <79444606+NguyenMaiNghiem@users.noreply.github.com>
2021-05-18 00:14:57 +0700

    Initial commit

---
+++ app/src/main/java/com/nguyenmainghiem/relativelayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nguyenmainghiem.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //To là ngược chiều
+        //Align là cùng chiều
+    }
+}
